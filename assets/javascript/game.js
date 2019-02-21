506202ad00319d1b671523d7c5a6d11a71ad3983
Jose Torres <jt227b@gmail.com>
2019-02-21 17:08:12 -0600

    Troubleshoot JavaScript Not Pushing Win/Lose Alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 // Variables //
-// ======================================================================================= //
-
+// ================================================================================ //
 
 var wordsList = ["nintendo", "yoshi", "bowser", "luigi", "wario", "koopalings", "mario", "princess", "donkey kong", "goomba"];
 var chosenWord = "";
@@ -10,63 +9,62 @@ var blanksAndSuccesses = [];
 var wrongGuesses = [];
 var letterGuessed = "";
 
-// Counter //
-// ======================================================================================= //
-
+// Counters //
+// ================================================================================ //
 var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
 
-
-// Functions //
-// ======================================================================================= //
-
+// Function //
+// ================================================================================ //
 function startGame() {
 
+
     numGuesses = 9;
-
     chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
-
-    letterInChosenWord = chosenWord.split("");
-
-    numBlanks = letterInChosenWord.length;
-
+    lettersInChosenWord = chosenWord.split("");
+    numBlanks = lettersInChosenWord.length;
     console.log(chosenWord);
-
     blanksAndSuccesses = [];
-
     wrongGuesses = [];
 
     for (var i = 0; i < numBlanks; i++) {
         blanksAndSuccesses.push("_");
     }
 
+    // Print in console //
+    // ================================================================================ //
     console.log(blanksAndSuccesses);
+
 
     document.getElementById("guesses-left").innerHTML = numGuesses;
     document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
     document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-
-
 }
 
 function checkLetters(letter) {
 
     var letterInWord = false;
 
+
     for (var i = 0; i < numBlanks; i++) {
+
         if (chosenWord[i] === letter) {
+
             letterInWord = true;
         }
     }
 
+
     if (letterInWord) {
+
+        // Loop //
+        // ================================================================================ //
         for (var j = 0; j < numBlanks; j++) {
+
             if (chosenWord[j] === letter) {
 
                 blanksAndSuccesses[j] = letter;
-
-
             }
         }
 
@@ -74,53 +72,62 @@ function checkLetters(letter) {
     }
 
     else {
+
         wrongGuesses.push(letter);
 
         numGuesses--;
+
     }
 
 }
 
+// Function //
+// ================================================================================ //
+
 function roundComplete() {
 
+
     console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+
 
     document.getElementById("guesses-left").innerHTML = numGuesses;
     document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
     document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
+
     if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+
+
         winCounter++;
 
         // Win Alert //
-        // =======================================================================================
+        // ================================================================================ //
 
-        alert("It's-A Me, Mario!");
-
+        alert("It's-A Me, Mario! You Win");
         document.getElementById("win-counter").innerHTML = winCounter;
-
         startGame();
     }
+
 
     else if (numGuesses === 0) {
         lossCounter++;
 
         // Lose Alert //
-        // ======================================================================================= //
+        // ================================================================================ //
 
-        alert("Oopsy-daisy! You Lose");
-
+        alert("Mamma mia! You lose");
         document.getElementById("loss-counter").innerHTML = lossCounter;
-
         startGame();
+
     }
 
 }
 
-// Main Functions //
-// ======================================================================================= //
+// Function //
+// ================================================================================ //
 
 startGame();
+
 document.onkeyup = function (event) {
     letterGuessed = String.fromCharCode(event.which).toLowerCase();
     checkLetters(letterGuessed);
