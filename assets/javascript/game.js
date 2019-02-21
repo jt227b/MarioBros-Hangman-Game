cd5acd64cb69fe36ee32b45c99d5e3ff37cfe8a2
Jose Torres <jt227b@gmail.com>
2019-02-20 23:19:27 -0600

    Added second page with game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,21 @@
+// VARIABLES
+// =======================================================================================
+
+// Various Arrays
+
+
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wordGuess = ["nintendo","yoshi", "bowser", "luigi", "wario", "koopalings", "mario","princess","donkey kong","goomba"];
+var wordGuess = ["nintendo", "yoshi", "bowser", "luigi", "wario", "koopalings", "mario", "princess", "donkey kong", "goomba"];
 
 
 
 
 
-function startGame(){
+function startGame() {
 
-    
+
 }
